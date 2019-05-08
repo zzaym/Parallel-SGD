0a5c572ed814e3c1e29f1ab8ee4a4edecc325164
Duuuuuu <djzdevin@foxmail.com>
2019-05-08 17:50:18 -0400

    change tab to space

--- src/seq_sgd/seq_sgd.cpp
+++ src/seq_sgd/seq_sgd.cpp
@@ -1,5 +1,5 @@
 /*
-	Sequential version of SGD
+    Sequential version of SGD
 */
 
 #include "seq_sgd.hpp"
@@ -9,86 +9,86 @@
 
 // constructor
 seq_sgd::seq_sgd(double (*obj)(const std::vector<double>&, \
-			const std::vector< std::vector<double> >&, const std::vector<double>&), \
-			std::vector<double> (*obj_grad)(const std::vector<double>&, \
-			const std::vector< std::vector<double> >&, const std::vector<double>&), \
-			const std::vector<double>& init_weights, \
-			const std::vector< std::vector<double> >& X,\
-			const std::vector<double>& Y, \
-			const float lr, \
-			const unsigned int num_iters, \
-			const unsigned int num_batches) {
-	objective = obj;
-	gradient = obj_grad;
-	weights = init_weights;
-	this->lr = lr;
-	this->num_iters = num_iters;
-	m = num_batches;
-	N = X.size();
-	d = X[0].size();
-	b = static_cast<int>(N/m);
-	// may be discarded if no memory
-	this->X = X;
-	this->Y = Y;
+            const std::vector< std::vector<double> >&, const std::vector<double>&), \
+            std::vector<double> (*obj_grad)(const std::vector<double>&, \
+            const std::vector< std::vector<double> >&, const std::vector<double>&), \
+            const std::vector<double>& init_weights, \
+            const std::vector< std::vector<double> >& X,\
+            const std::vector<double>& Y, \
+            const float lr, \
+            const unsigned int num_iters, \
+            const unsigned int num_batches) {
+    objective = obj;
+    gradient = obj_grad;
+    weights = init_weights;
+    this->lr = lr;
+    this->num_iters = num_iters;
+    m = num_batches;
+    N = X.size();
+    d = X[0].size();
+    b = static_cast<int>(N/m);
+    // may be discarded if no memory
+    this->X = X;
+    this->Y = Y;
 
-	// shuffle indices
-	std::vector<int> indices(N);
-	for (unsigned int i = 0; i < N; i++)
-		indices[i] = i;
-	std::random_shuffle(indices.begin(), indices.end());
+    // shuffle indices
+    std::vector<int> indices(N);
+    for (unsigned int i = 0; i < N; i++)
+        indices[i] = i;
+    std::random_shuffle(indices.begin(), indices.end());
 
-	// init batches
-	int index = 0;
-	Xs = std::vector< std::vector< std::vector<double> > >(m, \
-						std::vector< std::vector<double> >(b, std::vector<double>(d)));
-	Ys = std::vector< std::vector<double> >(m, std::vector<double>(b));
-	for (unsigned int i = 0; i < m; i++) {
-		for (unsigned int j = 0; j < b; j++) {
-			for (unsigned int k = 0; k < d; k++) 
-				Xs[i][j][k] = X[indices[index]][k];
-			Ys[i][j] = Y[indices[index]];
-			index++;
-		}
-	}	
+    // init batches
+    int index = 0;
+    Xs = std::vector< std::vector< std::vector<double> > >(m, \
+                        std::vector< std::vector<double> >(b, std::vector<double>(d)));
+    Ys = std::vector< std::vector<double> >(m, std::vector<double>(b));
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < b; j++) {
+            for (unsigned int k = 0; k < d; k++) 
+                Xs[i][j][k] = X[indices[index]][k];
+            Ys[i][j] = Y[indices[index]];
+            index++;
+        }
+    }    
 }
 
 
 seq_sgd* seq_sgd::clone() const {
-	return new seq_sgd(*this);
+    return new seq_sgd(*this);
 }
 
 seq_sgd::~seq_sgd(){}
 
 
 void seq_sgd::update(const unsigned int print_every) {
-	for (unsigned int i = 0; i < num_iters; ++i) {
-		int index = std::rand() % m;
+    for (unsigned int i = 0; i < num_iters; ++i) {
+        int index = std::rand() % m;
 
-		// compute gradient
-		std::vector<double> weights_grad(gradient(weights, Xs[index], Ys[index]));
+        // compute gradient
+        std::vector<double> weights_grad(gradient(weights, Xs[index], Ys[index]));
 
-		// multiply by lr
-		std::transform(weights_grad.begin(), weights_grad.end(), weights_grad.begin(), \
-			std::bind(std::multiplies<double>(), std::placeholders::_1, -lr));
+        // multiply by lr
+        std::transform(weights_grad.begin(), weights_grad.end(), weights_grad.begin(), \
+            std::bind(std::multiplies<double>(), std::placeholders::_1, -lr));
 
-		// descent
-		std::transform(weights.begin(), weights.end(), \
-			weights_grad.begin(), weights.begin(), std::plus<double>());
+        // descent
+        std::transform(weights.begin(), weights.end(), \
+            weights_grad.begin(), weights.begin(), std::plus<double>());
 
-		// verbose info
-		if (print_every > 0 && i % print_every == 0) {
-			std::cout << "Iteration " << i << ", loss: " << get_loss() << std::endl;
-		}
-	}
+        // verbose info
+        if (print_every > 0 && i % print_every == 0) {
+            std::cout << "Iteration " << i << ", loss: " << get_loss() << std::endl;
+        }
+    }
 } 
 
 
 std::vector<double> seq_sgd::get_weights() {
-	return weights;
+    return weights;
 }
 
 
 double seq_sgd::get_loss() {
-	loss = objective(weights, X, Y);
-	return loss;
+    loss = objective(weights, X, Y);
+    return loss;
 }
--- src/seq_sgd/seq_sgd.hpp
+++ src/seq_sgd/seq_sgd.hpp
@@ -1,5 +1,5 @@
 /*
-	Sequential version of SGD
+    Sequential version of SGD
 */
 
 #ifndef SSGD_H
@@ -11,56 +11,56 @@
 
 class seq_sgd {
 public:
-	// constructor
-	seq_sgd();
-	seq_sgd(double (*obj)(const std::vector<double>&, \
-		const std::vector< std::vector<double> >&, const std::vector<double>&), \
-		std::vector<double> (*obj_grad)(const std::vector<double>&, \
-		const std::vector< std::vector<double> >&, const std::vector<double>&), \
-		const std::vector<double>& init_weights,
-		const std::vector< std::vector<double> >& X,\
-		const std::vector<double>& Y, \
-		const float lr=0.01, \
-		const unsigned int num_iters=1000, \
-		const unsigned int num_batches=64);
+    // constructor
+    seq_sgd();
+    seq_sgd(double (*obj)(const std::vector<double>&, \
+        const std::vector< std::vector<double> >&, const std::vector<double>&), \
+        std::vector<double> (*obj_grad)(const std::vector<double>&, \
+        const std::vector< std::vector<double> >&, const std::vector<double>&), \
+        const std::vector<double>& init_weights,
+        const std::vector< std::vector<double> >& X,\
+        const std::vector<double>& Y, \
+        const float lr=0.01, \
+        const unsigned int num_iters=1000, \
+        const unsigned int num_batches=64);
 
-	// clone
-	seq_sgd* clone() const;
+    // clone
+    seq_sgd* clone() const;
 
-	// destructor
-	~seq_sgd();
+    // destructor
+    ~seq_sgd();
 
-	// functions
-	// perform SGD
-	virtual void update(const unsigned int print_every=0);
+    // functions
+    // perform SGD
+    virtual void update(const unsigned int print_every=0);
 
-	// get weights
-	std::vector<double> get_weights();
+    // get weights
+    std::vector<double> get_weights();
 
-	// get loss
-	double get_loss();
+    // get loss
+    double get_loss();
 
 protected:
-	// variables
-	std::vector< std::vector< std::vector<double> > > Xs;
-	std::vector< std::vector<double> > Ys;
-	std::vector<double> weights;
-	float lr;
-	unsigned int num_iters;
-	unsigned int m; // the number of batch
-	unsigned int N; // the number of input samples
-	unsigned int d; // the dims of input
-	unsigned int b; // the batch size
-	double loss;
-	// may be discarded if no memory
-	std::vector< std::vector<double> > X; 
-	std::vector<double> Y;
+    // variables
+    std::vector< std::vector< std::vector<double> > > Xs;
+    std::vector< std::vector<double> > Ys;
+    std::vector<double> weights;
+    float lr;
+    unsigned int num_iters;
+    unsigned int m; // the number of batch
+    unsigned int N; // the number of input samples
+    unsigned int d; // the dims of input
+    unsigned int b; // the batch size
+    double loss;
+    // may be discarded if no memory
+    std::vector< std::vector<double> > X; 
+    std::vector<double> Y;
 
-	// functions
-	double (*objective)(const std::vector<double>&, \
-		const std::vector< std::vector<double> >&, const std::vector<double>&);
-	std::vector<double> (*gradient)(const std::vector<double>&, \
-		const std::vector< std::vector<double> >&, const std::vector<double>&);
+    // functions
+    double (*objective)(const std::vector<double>&, \
+        const std::vector< std::vector<double> >&, const std::vector<double>&);
+    std::vector<double> (*gradient)(const std::vector<double>&, \
+        const std::vector< std::vector<double> >&, const std::vector<double>&);
 };
 
 #endif
